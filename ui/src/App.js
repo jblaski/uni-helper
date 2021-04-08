135302c181031d2b7e2e4af9d0776391d23f03e5
Julian Blaskiewicz <bd021726@student.reading.ac.uk>
2021-04-08 20:12:31 +0100

    Added material-ui, trying out autocomplete

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,22 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { Button } from '@material-ui/core';
+import TextField from '@material-ui/core/TextField';
+import Autocomplete from '@material-ui/lab/Autocomplete';
 function App() {
+
+  // Will get these from API
+  const top100Films = [
+    { title: 'London', year: 1994 },
+    { title: 'Birmingham', year: 1972 },
+    { title: 'Bristol', year: 1974 },
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="Search-area">
+        <p>welcome to uni-helper!</p>
+        <p>search for a town:</p>
+      
+        <Autocomplete
+          id="combo-box-demo"
+          options={top100Films}
+          getOptionLabel={(option) => option.title}
+          style={{ width: 300 }}
+          renderInput={(params) => <TextField {...params} label="Town" variant="outlined" />}
+        />
       </header>
     </div>
   );
